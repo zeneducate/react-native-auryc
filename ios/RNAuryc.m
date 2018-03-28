440358640f687633889cf10501486249609f6491
Feng Shao <feng.shao@auryc.com>
2018-03-28 14:21:43 -0700

    updating configs

--- ios/RNAuryc.m
+++ ios/RNAuryc.m
@@ -1,13 +1,32 @@
 
 #import "RNAuryc.h"
+#import <Auryc-Swift.h>
 
 @implementation RNAuryc
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+RCT_EXPORT_MODULE(RNAuryc)
+
+RCT_EXPORT_METHOD(initialize: (NSString *) apiKey siteId: (NSString *)siteId) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [Auryc initialize: apiKey siteId: siteId];
+    });
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(identify: (NSString *) identity){
+    [[Auryc mainInstance] identify: identity];
+}
+
+RCT_EXPORT_METHOD(addUserProperties:  (NSDictionary *) properties){
+    [[Auryc mainInstance] addUserProperties: properties];
+}
+
+RCT_EXPORT_METHOD(addSessionProperties:  (NSDictionary *) properties){
+    [[Auryc mainInstance] addSessionProperties: properties];
+}
+
+RCT_EXPORT_METHOD(track: (NSString *) eventName properties: (NSDictionary *) properties){
+    [[Auryc mainInstance] track: eventName properties: properties];
+}
 
 @end
   
