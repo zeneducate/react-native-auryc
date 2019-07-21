3095b62c8fe281d6c716eb0e6f7cd5f0f53f5ce5
Alcides Zelaya <alcideszelaya@hotmail.com>
2019-07-21 18:01:04 +0200

    include AurycSDK

--- ios/Pods/Target Support Files/AurycSDK/AurycSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AurycSDK : NSObject
-@end
-@implementation PodsDummy_AurycSDK
-@end
--- ios/RNAuryc.h
+++ ios/RNAuryc.h
@@ -1,3 +1,16 @@
+
+//#if __has_include("RCTBridgeModule.h")
+//#import "RCTBridgeModule.h"
+//#else
+//#import <React/Base/RCTBridgeModule.h>
+//#endif
+
+//#if __has_include("RCTUIManager.h")
+//#import "RCTUIManager.h"
+//#else
+//#import <React/Modules/RCTUIManager.h>
+//#endif
+
 #import  <React/RCTBridgeModule.h>
 #import  <React/RCTUIManager.h>
 
